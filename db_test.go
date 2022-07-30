060c59eb93ccac6ecca596bc80ccad3242b3142a
euraxluo <1793939176@qq.com>
2022-07-30 23:48:03 +0800

    v0.1.3 merge

--- db_test.go
+++ db_test.go
@@ -1,7 +1,11 @@
 package bitcask
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
+	"time"
 )
 
 func TestDB_Basic(t *testing.T) {
@@ -93,6 +97,78 @@ func TestDB_Basic(t *testing.T) {
 			return nil
 		}); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestDB_Merge_For_string(t *testing.T) {
+	fileDir := "./data"
+
+	files, _ := ioutil.ReadDir(fileDir)
+	for _, f := range files {
+		name := f.Name()
+		if name != "" {
+			err := os.RemoveAll(fileDir + "/" + name)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	opt := DefaultOptions
+	opt.Dir = fileDir
+	opt.SegmentSize = 1 * 100
+
+	db2, err := Open(opt)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bucketForString := "test_merge"
+
+	key1 := []byte("key_" + fmt.Sprintf("%07d", 1))
+	value1 := []byte("value1value1value1value1value1")
+	if err := db2.Update(
+		func(tx *Tx) error {
+			return tx.Put(bucketForString, key1, value1, Persistent)
+		}); err != nil {
+		t.Error("initStringDataAndDel,err batch put", err)
+	}
+
+	key2 := []byte("key_" + fmt.Sprintf("%07d", 2))
+	value2 := []byte("value2value2value2value2value2")
+	if err := db2.Update(
+		func(tx *Tx) error {
+			return tx.Put(bucketForString, key2, value2, Persistent)
+		}); err != nil {
+		t.Error("initStringDataAndDel,err batch put", err)
+	}
+
+	if err := db2.Update(
+		func(tx *Tx) error {
+			return tx.Delete(bucketForString, key2)
+		}); err != nil {
+		t.Error(err)
+	}
+
+	if err := db2.View(
+		func(tx *Tx) error {
+			if _, err := tx.Get(bucketForString, key2); err == nil {
+				t.Error("err read data ", err)
+			}
+			return nil
+		}); err != nil {
+		t.Fatal(err)
+	}
+
+	//GetValidKeyCount
+	//validKeyNum := db2.BPTreeIdx[bucketForString].ValidKeyCount
+	//if validKeyNum != 1 {
+	//	t.Errorf("err GetValidKeyCount. got %d want %d", validKeyNum, 1)
+	//}
+	time.Sleep(1)
+	if err = db2.Merge(); err != nil {
+		t.Error("err merge", err)
 	}
 }
 
